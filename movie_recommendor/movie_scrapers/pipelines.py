481900c57c81d8b4901b94d78e1ebbe463553c73
Devanshu Singla <devanshu803.singla@gmail.com>
2021-05-28 18:36:27 +0530

    first working model

--- movie_recommendor/movie_scrapers/pipelines.py
+++ movie_recommendor/movie_scrapers/pipelines.py
@@ -5,12 +5,17 @@
 
 
 # useful for handling different item types with a single interface
+import json
 from itemadapter import ItemAdapter
 from scrapy.exceptions import DropItem
+from ..google_verifier import GoogleVerifier
+
 
 class MovieScrapersPipeline:
     def __init__(self):
         self.ids_seen = set()
+        self.gv = GoogleVerifier()
+        self.f = open('data/movies.jl', 'w')
     
     def process_item(self, item, spider):
         adapter = ItemAdapter(item)
@@ -18,4 +23,11 @@ class MovieScrapersPipeline:
             DropItem(f"Dupticate item found: {item!r}")
         else:
             self.ids_seen.add((adapter['name'],adapter['release_date']))
-            return item
+            if self.gv.verify(item):
+                self.f.write(json.dumps(ItemAdapter(item).asdict())+'\n')
+                return item
+            else:
+                DropItem('Failed as a good movie by given criterion')
+    
+    def __del__(self):
+        self.f.close()
